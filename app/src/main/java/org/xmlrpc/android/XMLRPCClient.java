121b3452f598edaf574b3eaa34e5b513226627f4
Alex Trofimov <alex.trofimov@boonex.com>
2016-11-25 05:49:57 +0600

    #507 HTTPS URLs support, by default letsencrypt root certificate is included

--- app/src/main/java/org/xmlrpc/android/XMLRPCClient.java
+++ app/src/main/java/org/xmlrpc/android/XMLRPCClient.java
@@ -1,31 +1,62 @@
 package org.xmlrpc.android;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.Reader;
 import java.io.StringWriter;
+import java.net.Socket;
 import java.net.URI;
 import java.net.URL;
+import java.security.KeyManagementException;
+import java.security.KeyStore;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+import java.security.UnrecoverableKeyException;
+import java.security.cert.CertificateException;
+import java.security.cert.X509Certificate;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Map;
 
 import org.apache.http.Header;
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
 import org.apache.http.HttpStatus;
+import org.apache.http.HttpVersion;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.client.params.HttpClientParams;
+import org.apache.http.conn.ClientConnectionManager;
+import org.apache.http.conn.scheme.PlainSocketFactory;
+import org.apache.http.conn.scheme.Scheme;
+import org.apache.http.conn.scheme.SchemeRegistry;
+import org.apache.http.conn.ssl.SSLSocketFactory;
+import org.apache.http.conn.ssl.X509HostnameVerifier;
 import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.impl.conn.SingleClientConnManager;
+import org.apache.http.impl.conn.tsccm.ThreadSafeClientConnManager;
+import org.apache.http.params.BasicHttpParams;
 import org.apache.http.params.HttpParams;
 import org.apache.http.params.HttpProtocolParams;
+import org.apache.http.protocol.HTTP;
 import org.xmlpull.v1.XmlPullParser;
 import org.xmlpull.v1.XmlPullParserFactory;
 import org.xmlpull.v1.XmlSerializer;
 
 import com.boonex.oo.Main;
+import com.boonex.oo.R;
 
 import android.util.Xml;
 
+import javax.net.ssl.HostnameVerifier;
+import javax.net.ssl.HttpsURLConnection;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.TrustManagerFactory;
+import javax.net.ssl.X509TrustManager;
 
 /**
  * XMLRPCClient allows to call remote XMLRPC method.
@@ -70,6 +101,109 @@ import android.util.Xml;
  * </p>
  */
 
+/**
+ * http://stackoverflow.com/questions/2642777/trusting-all-certificates-using-httpclient-over-https/6378872#6378872
+ * http://blog.crazybob.org/2010/02/android-trusting-ssl-certificates.html
+ * Allows you to trust certificates from additional KeyStores in addition to
+ * the default KeyStore
+ */
+class AdditionalKeyStoresSSLSocketFactory extends SSLSocketFactory {
+	protected SSLContext sslContext = SSLContext.getInstance("TLS");
+
+	public AdditionalKeyStoresSSLSocketFactory(KeyStore keyStore) throws NoSuchAlgorithmException, KeyManagementException, KeyStoreException, UnrecoverableKeyException {
+		super(null, null, null, null, null, null);
+		sslContext.init(null, new TrustManager[]{new AdditionalKeyStoresTrustManager(keyStore)}, null);
+	}
+
+	@Override
+	public Socket createSocket(Socket socket, String host, int port, boolean autoClose) throws IOException {
+		return sslContext.getSocketFactory().createSocket(socket, host, port, autoClose);
+	}
+
+	@Override
+	public Socket createSocket() throws IOException {
+		return sslContext.getSocketFactory().createSocket();
+	}
+
+
+
+	/**
+	 * Based on http://download.oracle.com/javase/1.5.0/docs/guide/security/jsse/JSSERefGuide.html#X509TrustManager
+	 */
+	public static class AdditionalKeyStoresTrustManager implements X509TrustManager {
+
+		protected ArrayList<X509TrustManager> x509TrustManagers = new ArrayList<X509TrustManager>();
+
+
+		protected AdditionalKeyStoresTrustManager(KeyStore... additionalkeyStores) {
+			final ArrayList<TrustManagerFactory> factories = new ArrayList<TrustManagerFactory>();
+
+			try {
+				// The default Trustmanager with default keystore
+				final TrustManagerFactory original = TrustManagerFactory.getInstance(TrustManagerFactory.getDefaultAlgorithm());
+				original.init((KeyStore) null);
+				factories.add(original);
+
+				for( KeyStore keyStore : additionalkeyStores ) {
+					final TrustManagerFactory additionalCerts = TrustManagerFactory.getInstance(TrustManagerFactory.getDefaultAlgorithm());
+					additionalCerts.init(keyStore);
+					factories.add(additionalCerts);
+				}
+
+			} catch (Exception e) {
+				throw new RuntimeException(e);
+			}
+
+
+
+            /*
+             * Iterate over the returned trustmanagers, and hold on
+             * to any that are X509TrustManagers
+             */
+			for (TrustManagerFactory tmf : factories)
+				for( TrustManager tm : tmf.getTrustManagers() )
+					if (tm instanceof X509TrustManager)
+						x509TrustManagers.add( (X509TrustManager)tm );
+
+
+			if( x509TrustManagers.size()==0 )
+				throw new RuntimeException("Couldn't find any X509TrustManagers");
+
+		}
+
+		/*
+         * Delegate to the default trust manager.
+         */
+		public void checkClientTrusted(X509Certificate[] chain, String authType) throws CertificateException {
+			final X509TrustManager defaultX509TrustManager = x509TrustManagers.get(0);
+			defaultX509TrustManager.checkClientTrusted(chain, authType);
+		}
+
+		/*
+         * Loop over the trustmanagers until we find one that accepts our server
+         */
+		public void checkServerTrusted(X509Certificate[] chain, String authType) throws CertificateException {
+			for( X509TrustManager tm : x509TrustManagers ) {
+				try {
+					tm.checkServerTrusted(chain,authType);
+					return;
+				} catch( CertificateException e ) {
+					// ignore
+				}
+			}
+			throw new CertificateException();
+		}
+
+		public X509Certificate[] getAcceptedIssuers() {
+			final ArrayList<X509Certificate> list = new ArrayList<X509Certificate>();
+			for( X509TrustManager tm : x509TrustManagers )
+				list.addAll(Arrays.asList(tm.getAcceptedIssuers()));
+			return list.toArray(new X509Certificate[list.size()]);
+		}
+	}
+
+}
+
 public class XMLRPCClient {	
 	private static final String TAG_METHOD_CALL = "methodCall";
 	private static final String TAG_METHOD_NAME = "methodName";
@@ -98,13 +232,44 @@ public class XMLRPCClient {
 		HttpParams params = postMethod.getParams();
 		HttpProtocolParams.setUseExpectContinue(params, false);		
 				
-		client = new DefaultHttpClient();		
+		client = this.createHttpClient();
 		HttpParams paramsClient = client.getParams();
 		HttpClientParams.setRedirecting(paramsClient, false); // manage redirects manually
 		
 		serializer = Xml.newSerializer();
 	}
-	
+
+	private SSLSocketFactory newSslSocketFactory() {
+		try {
+			KeyStore trusted = KeyStore.getInstance("BKS");
+			InputStream in = Main.MainActivity.getResources().openRawResource(R.raw.letsencrypt); // http://stackoverflow.com/questions/2642777/trusting-all-certificates-using-httpclient-over-https/6378872#6378872
+			try {
+				trusted.load(in, "ez24get".toCharArray());
+			} finally {
+				in.close();
+			}
+			return new AdditionalKeyStoresSSLSocketFactory(trusted);
+		} catch (Exception e) {
+			throw new AssertionError(e);
+		}
+	}
+
+	private HttpClient createHttpClient()
+	{
+		SchemeRegistry registry = new SchemeRegistry();
+		registry.register(new Scheme("http", PlainSocketFactory.getSocketFactory(), 80));
+		registry.register(new Scheme("https", newSslSocketFactory(), 443));
+
+		HttpParams params = new BasicHttpParams();
+		HttpProtocolParams.setVersion(params, HttpVersion.HTTP_1_1);
+		HttpProtocolParams.setContentCharset(params, HTTP.DEFAULT_CONTENT_CHARSET);
+		HttpProtocolParams.setUseExpectContinue(params, true);
+
+		ClientConnectionManager conMgr = new ThreadSafeClientConnManager(params, registry);
+
+		return new DefaultHttpClient(conMgr, params);
+	}
+
 	/**
 	 * Convenience constructor. Creates new instance based on server String address
 	 * @param XMLRPC server address
